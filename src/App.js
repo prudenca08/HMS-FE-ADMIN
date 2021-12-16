affe77e973952575862faa9dd43622bbbbd0fcf6
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2021-12-16 12:37:19 +0700

    cek login

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          cek login cek
         </a>
       </header>
     </div>
