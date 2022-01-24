dac4866d8d37b7dbd6ddc73fe7e5efdf05efb2b0
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2022-01-24 12:03:58 +0700

    update

--- src/config/redux/action/index.jsx
+++ src/config/redux/action/index.jsx
@@ -546,3 +546,4 @@ export const actionDeletePatSchedule = (data) => (dispatch) => {
       });
   });
 };
+
--- src/pages/updatePassword/UpdatePassword.jsx
+++ src/pages/updatePassword/UpdatePassword.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./updatePassword.css";
 import Password from "../../assets/img/icon/passPage.png";
+import { useState } from "react";
+import Loading from "../../assets/img/icon/loading.gif";
 
 const UpdatePassword = () => {
+  const [field, setField] = useState({
+    newpassword : "",
+    confirmpassword : "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleOnChange = (event) => {
+    let {name, value} = event.currentTarget;
+    setField({...field, [name] : value});
+    console.log(field);
+  }
+
   return (
     <div className="widget">
       <h3 className="widgetTitle pass">Change Password</h3>
@@ -16,21 +31,25 @@ const UpdatePassword = () => {
           <label>New Password</label>
           <input
             type="password"
-            name="newpass"
-            value=""
+            name="newpassword"
+            value={field.newpassword}
             className="form-control"
+            onChange={handleOnChange}
           />
         </div>
         <div className="changePassItem">
         <label>Confirm Password</label>
           <input
             type="password"
-            name="confpass"
-            value=""
+            name="confirmpassword"
+            value={field.confirmpassword}
             className="form-control"
+            onChange={handleOnChange}
           />
         </div>
-        <button className="btnSavePass">Save</button>
+        <button className="btnSavePass" disabled={isLoading}>
+          {isLoading ? <img src={Loading} alt="" width="40px" /> : "Save"}
+        </button>
         </form>
       </div>
       </div>
