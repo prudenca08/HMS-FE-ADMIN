732d48b544db94bfae5d44e345e288b4e7717379
Prudenca Daffa <prudencaahdaffa08@gmail.com>
2022-01-25 17:47:25 +0700

    dashboard

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,56 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./widget.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { outpatientRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { actionGetAllOutpatients } from "../../config/redux/action";
+import { connect } from "react-redux";
 
-export default function Widget() {
+const Widget = (props) => {
+  const [data, setData] = useState(outpatientRows);
+
+  useEffect(() => {
+    if (props.outpatient.length <= 0) {
+      props.AllOutpatient().then(() => {
+        console.log(props.outpatient);
+      });
+    } else {
+      setData(props.outpatient);
+    }
+  }, [props]);
+
+  const columns = [
+    { field: "nik", headerName: "NIK", width: 120 },
+    { field: "patientName", headerName: "Patient Name", width: 180 },
+    { field: "day", headerName: "Day", width: 100 },
+    { field: "date", headerName: "Date", width: 100 },
+    { field: "time", headerName: "Time", width: 120 },
+    { field: "nip", headerName: "NIP", width: 120 },
+    { field: "doctor_name", headerName: "Doctor", width: 170 },
+    { field: "room", headerName: "Room", width: 100 },
+    { field: "symptoms", headerName: "Symptoms", width: 150 },
+    { field: "title", headerName: "Diagnosis", width: 150 },
+    { field: "detailrecipe", headerName: "Medicine Recipe", width: 150 },
+    { field: "status", headerName: "Status", width: 100 },
+  ];
+
   return (
     <div className="widget">
-      <h3 className="widgetTitle">Outpatient Sessions</h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Patient Name</th>
-            <th scope="col">Day</th>
-            <th scope="col">Date</th>
-            <th scope="col">Time</th>
-            <th scope="col">Doctor</th>
-            <th scope="col">Symptoms</th>
-            <th scope="col">Room</th>
-            <th scope="col">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Margareth Ellie</td>
-            <td>Selasa</td>
-            <td> 14 Des 2021</td>
-            <td> 13.00-13.30</td>
-            <td>dr.Thomas Harianja</td>
-            <td> Gatal-gatal, sesak nafas, demam, batuk, pilek...</td>
-            <td>A27</td>
-            <td>On Progress</td>
-          </tr>
-          <tr>
-            <td>Michael Jordan</td>
-            <td>Selasa</td>
-            <td> 14 Des 2021</td>
-            <td> 13.00-13.30</td>
-            <td>dr.Prima Simorangkir</td>
-            <td> Demam, badan kemerahan,Keringat dingin, batuk...</td>
-            <td>B02</td>
-            <td>Done</td>
-          </tr>
-          <tr>
-            <td>Hayati Sulivan</td>
-            <td>Selasa</td>
-            <td> 14 Des 2021</td>
-            <td> 13.00-13.30</td>
-            <td>dr.Thomas Harianja</td>
-            <td> Gatal-gatal, sesak nafas, demam, batuk, pilek...</td>
-            <td>A27</td>
-            <td>Waiting</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="outpatientList p-4">
+        <h1>Outpatient Session</h1>
+        {props.outpatient.length !== 0 && (
+          <DataGrid
+            rows={data}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[25]}
+            checkboxSelection
+            autoHeight={true}
+          />
+        )}
+      </div>
     </div>
   );
-}
+};
+const reduxState = (state) => ({
+  outpatient: state.outpatient,
+});
+const reduxDispatch = (dispatch) => ({
+  AllOutpatient: (data) => dispatch(actionGetAllOutpatients(data)),
+});
+
+export default connect(reduxState, reduxDispatch)(Widget);
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import DashInfo from '../../components/DashInfo/DashInfo'
-import Widget from '../../components/Widget/Widget'
-import "./dashboard.css"
+import React from "react";
+import DashInfo from "../../components/DashInfo/DashInfo";
+import Widget from "../../components/Widget/Widget";
+import "./dashboard.css";
 
 export default function Dashboard() {
-    return (
-        <div className='dashboard'>
-            <DashInfo />
-            <div className="dashWidget">
-                <Widget />
-            </div>
-        </div>
-    )
+  return (
+    <div className="dashboard">
+      <DashInfo />
+      <div className="dashWidget">
+        <Widget />
+      </div>
+    </div>
+  );
 }
